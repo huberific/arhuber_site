7b91960a291d7f5953e70fa75a1a0f0f7d91dec2
huberific <huber.aaron@gmail.com>
2022-03-28 08:55:06 -0700

    optimized

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+  target: 'node',
+  externals: [nodeExternals()],
+  externalsPresets: {
+      node: true
+  },
   // The entry point file described above
   entry: './src/public/js/habitTracker.js',
   // The location of the build folder described above
@@ -8,7 +14,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'habitTracker.bundle.js'
   },
+    mode: 'production'
   // Optional and for development only. This provides the ability to
   // map the built code back to the original source format when debugging.
-  devtool: 'eval-source-map',
+  // devtool: 'eval-source-map',
 };
